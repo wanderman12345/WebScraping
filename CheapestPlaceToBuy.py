6c4bfb47098ffd433de3444e556c6c364c56e190
Mathew Raju <mraju@ucsc.edu>
2022-07-07 16:46:08 -0700

    Working on cheapest place to Buy

--- CheapestPlaceToBuy.py
+++ CheapestPlaceToBuy.py
@@ -1,54 +1,88 @@
-from email import header
-import requests
-import smtplib
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 import time
+import sys
 
-#bestBuy
-#walMart
-#target
-#Costco
-#Amazon
-URL = "https://www.bestbuy.com/site/assassins-creed-valhalla-standard-edition-playstation-4-playstation-5/6412166.p?skuId=6412166"
+#Returns the price of an item
 
-headers = {'User-Agent' :  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+def searchItem(item):
 
-def find_cheapest_price():
-    pageBestBuy = requests.get(URL, headers = headers)
+    out = {}
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driverCraig = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    soupBestBuy = BeautifulSoup(pageBestBuy.content, 'html.parser')
+    driver.get("https://www.bestbuy.com/")
+    driverCraig.get("https://sfbay.craigslist.org/")
 
+    try:
+        searchCraig = driverCraig.find_element(By.ID, "query")
+        searchCraig.send_keys(item)
+        searchCraig.send_keys(Keys.RETURN)
+        time.sleep(5)
+        search = driver.find_element(By.ID, "gh-search-input" )
+        search.send_keys(item)
+        search.send_keys(Keys.RETURN)
 
-    title = soupBestBuy.find('h1')
-    priceBestBuy = soupBestBuy.find(class_ = "priceView-hero-price priceView-customer-price").get_text()
-    allPrices = []
-    converted_price = float(priceBestBuy[1:3])
+        time.sleep(60)
 
-    if (converted_price < 35):
-        send_mail()
-    print(converted_price)
+        priceCraig = driverCraig.find_element(By.CLASS_NAME, "result-price")
+        priceText = driver.find_element(By.CLASS_NAME, "pricing-price")
 
+        # print(parsePrice(priceCraig.text))
+        # print(parsePrice(priceText.text))
+        out[parsePrice(priceCraig.text)] = driverCraig.current_url
+        out[parsePrice(priceText.text)] = driver.current_url
 
-def send_mail():
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-    server.login('mathew2002raj@gmail.com', 'AuthenticationPassword')
-
-    subject = 'Price fell down!'
-    body = 'check the Amazon link: "https://www.bestbuy.com/site/assassins-creed-valhalla-standard-edition-playstation-4-playstation-5/6412166.p?skuId=6412166"'
-    msg = f"Subject: {subject}\n\n{body}"
-    server.sendmail(
-        'mathew2002raj@gmail.com',
-        'mathew2002raj@gmail.com',
-        msg
-    )
-    print('Hey Email has been successfully sent')
-    server.quit()
+        return process(out)
+    except:
+        print("Error")
 
 
-find_cheapest_price()
+def process(DictionaryValues):
+    smallestPrice = sys.maxsize
+    for eachPrice in DictionaryValues:
+        if eachPrice < smallestPrice:
+            smallestPrice = eachPrice
+
+
+    return smallestPrice, DictionaryValues[smallestPrice]
+
+
+def parsePrice(Price):
+    price = ""
+    for eachChar in Price:
+        if eachChar == ".":
+            break
+        if eachChar != "$":
+            price += eachChar
+    return int(price)
+
+
+
+#Main
+
+CheapestPrice, CheapestURL = searchItem("ps4 console")
+print(CheapestPrice)
+print(CheapestURL)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
